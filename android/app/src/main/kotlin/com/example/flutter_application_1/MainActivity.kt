ec3b2fb09484d32e42bad4d41b3fc27935c35a97
Fatmadogan33 <fatoss.dogn@gmail.com>
2023-01-13 19:09:33 +0300

    txt change

--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.flutter_application_1
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutterembedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 
